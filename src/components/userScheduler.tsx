3973fd2c5264e9f23d62a9d9bb4057b1f2c3baf7
tomaszbilka <bila4@op.pl>
2024-08-17 15:12:06 +0200

    add pl locale

--- src/components/userScheduler.tsx
+++ src/components/userScheduler.tsx
@@ -12,37 +12,152 @@ import {
 import { useState } from "react";
 import { VIEW_NAME } from "../utils/constants";
 
-const schedulerData = [
+const appointments = [
   {
-    startDate: "2018-11-01T09:45",
-    endDate: "2018-11-01T11:00",
-    title: "Meeting",
+    title: "Website Re-Design Plan",
+    startDate: new Date(2018, 6, 23, 9, 30),
+    endDate: new Date(2018, 6, 23, 11, 30),
   },
   {
-    startDate: "2018-11-01T12:00",
-    endDate: "2018-11-01T13:30",
-    title: "Go to a gym",
+    title: "Book Flights to San Fran for Sales Trip",
+    startDate: new Date(2018, 6, 23, 12, 0),
+    endDate: new Date(2018, 6, 23, 13, 0),
+  },
+  {
+    title: "Install New Router in Dev Room",
+    startDate: new Date(2018, 6, 23, 14, 30),
+    endDate: new Date(2018, 6, 23, 15, 30),
+  },
+  {
+    title: "Approve Personal Computer Upgrade Plan",
+    startDate: new Date(2018, 6, 24, 10, 0),
+    endDate: new Date(2018, 6, 24, 11, 0),
+  },
+  {
+    title: "Final Budget Review",
+    startDate: new Date(2018, 6, 24, 12, 0),
+    endDate: new Date(2018, 6, 24, 13, 35),
+  },
+  {
+    title: "New Brochures",
+    startDate: new Date(2018, 6, 24, 14, 30),
+    endDate: new Date(2018, 6, 24, 15, 45),
+  },
+  {
+    title: "Install New Database",
+    startDate: new Date(2018, 6, 25, 9, 45),
+    endDate: new Date(2018, 6, 25, 11, 15),
+  },
+  {
+    title: "Approve New Online Marketing Strategy",
+    startDate: new Date(2018, 6, 25, 12, 0),
+    endDate: new Date(2018, 6, 25, 14, 0),
+  },
+  {
+    title: "Upgrade Personal Computers",
+    startDate: new Date(2018, 6, 25, 15, 15),
+    endDate: new Date(2018, 6, 25, 16, 30),
+  },
+  {
+    title: "Customer Workshop",
+    startDate: new Date(2018, 6, 26, 11, 0),
+    endDate: new Date(2018, 6, 26, 12, 0),
+  },
+  {
+    title: "Prepare 2015 Marketing Plan",
+    startDate: new Date(2018, 6, 26, 11, 0),
+    endDate: new Date(2018, 6, 26, 13, 30),
+  },
+  {
+    title: "Brochure Design Review",
+    startDate: new Date(2018, 6, 26, 14, 0),
+    endDate: new Date(2018, 6, 26, 15, 30),
+  },
+  {
+    title: "Create Icons for Website",
+    startDate: new Date(2018, 6, 27, 10, 0),
+    endDate: new Date(2018, 6, 27, 11, 30),
+  },
+  {
+    title: "Upgrade Server Hardware",
+    startDate: new Date(2018, 6, 27, 14, 30),
+    endDate: new Date(2018, 6, 27, 16, 0),
+  },
+  {
+    title: "Submit New Website Design",
+    startDate: new Date(2018, 6, 27, 16, 30),
+    endDate: new Date(2018, 6, 27, 18, 0),
+  },
+  {
+    title: "Launch New Website",
+    startDate: new Date(2018, 6, 26, 12, 20),
+    endDate: new Date(2018, 6, 26, 14, 0),
+  },
+  {
+    title: "Website Re-Design Plan",
+    startDate: new Date(2018, 6, 16, 9, 30),
+    endDate: new Date(2018, 6, 16, 15, 30),
+  },
+  {
+    title: "Book Flights to San Fran for Sales Trip",
+    startDate: new Date(2018, 6, 16, 12, 0),
+    endDate: new Date(2018, 6, 16, 13, 0),
+  },
+  {
+    title: "Install New Database",
+    startDate: new Date(2018, 6, 17, 15, 45),
+    endDate: new Date(2018, 6, 18, 12, 15),
+  },
+  {
+    title: "Approve New Online Marketing Strategy",
+    startDate: new Date(2018, 6, 18, 12, 35),
+    endDate: new Date(2018, 6, 18, 14, 15),
+  },
+  {
+    title: "Upgrade Personal Computers",
+    startDate: new Date(2018, 6, 19, 15, 15),
+    endDate: new Date(2018, 6, 20, 20, 30),
+  },
+  {
+    title: "Prepare 2015 Marketing Plan",
+    startDate: new Date(2018, 6, 20, 20, 0),
+    endDate: new Date(2018, 6, 20, 13, 30),
+  },
+  {
+    title: "Brochure Design Review",
+    startDate: new Date(2018, 6, 20, 14, 10),
+    endDate: new Date(2018, 6, 20, 15, 30),
+  },
+  {
+    title: "Vacation",
+    startDate: new Date(2018, 5, 22),
+    endDate: new Date(2018, 6, 1),
+  },
+  {
+    title: "Vacation",
+    startDate: new Date(2018, 6, 28),
+    endDate: new Date(2018, 7, 7),
   },
 ];
 
 function UserScheduler() {
-  const [data, setData] = useState(schedulerData);
+  const [data, setData] = useState(appointments);
   const [currentViewName, setCurrentViewName] = useState<string>(
     VIEW_NAME.month
   );
 
   return (
     <Paper>
-      <Scheduler data={data}>
+      <Scheduler data={data} locale="pl">
         <ViewState
-          defaultCurrentDate="2018-07-25"
           currentViewName={currentViewName}
+          defaultCurrentDate="2018-07-25"
           onCurrentViewNameChange={(name) => setCurrentViewName(name)}
         />
 
-        <WeekView startDayHour={10} endDayHour={19} />
-        <MonthView />
-        <DayView />
+        <WeekView displayName="Tydzień" endDayHour={19} startDayHour={10} />
+        <MonthView displayName="Miesiąc" />
+        <DayView displayName="Dzień" />
 
         <Toolbar />
         <ViewSwitcher />
